0072cc4cec8f2ea04da2cc928a3f5d69b50de9e5
Lui Jorge <Dragonexpert32@gmail.com>
2026-05-31 01:52:21 -0500

    fix: #B-05 corregir longitud de campo nombre

--- backend/src/main/java/com/mamamia/backend/model/model.java
+++ backend/src/main/java/com/mamamia/backend/model/model.java
@@ -9,7 +9,7 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(length = 100)
     private String nombre;
     private String descripcion;
 
